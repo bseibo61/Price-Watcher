4bcb4740d1c821eebe995822564100b3cdee8ff1
bseibo61 <brseibol@buffalo.edu>
2018-11-27 16:45:44 -0500

    logging current files

--- sparkAvg.py
+++ sparkAvg.py
@@ -6,6 +6,8 @@ from pyspark.sql.types import Row
 from pyspark.ml.feature import CountVectorizer
 from operator import add
 from pyspark.sql.window import Window
+from os import listdir
+from os.path import isfile, join
 import pyspark.sql.functions as func
 
 import pandas as pd
@@ -17,23 +19,38 @@ if __name__ == "__main__":
 		.appName("TfIdfExample")\
 		.getOrCreate()
 
-	sentenceData = spark.createDataFrame([
-		(0.0, "Hi hello hi hi ifs"),
-		(0.0, "I wish Java could hi hi hi")
-	], ["label", "sentence"])
-
-	sentenceData.show()
-
 	sc = SparkContext.getOrCreate()
-
-	#dirPath = "data/test/"
-
-	#textFiles = sc.textFile(dirPath)
 	
-	spark.read.format("csv").option("header", "true").load("output/*.csv")
-	df = spark.read.option("header","true").csv("output/*.csv")
-	print("\nThe data frame is: ",df,"\n")
-	df.show(n=500)
+	inputPath = "output"
+	filelist = [f for f in listdir(inputPath) if isfile(join(inputPath,f))]
+	print("\n Input files are",filelist,"\n")
+	
+	d = dict.fromkeys(filelist)
+
+	for f in filelist:
+	
+		print("\n INPUT FILE DATE IS:",f,"\n")		
+
+		spark.read.format("csv").option("header", "true").load("output/"+f)
+		df = spark.read.option("header","true").csv("output/"+f)
+		print("\nThe data frame is: ",df,"\n")
+	
+		df.createOrReplaceTempView("table")
+		tickers = {"AMD": (0,0), "TLRY": (0,0), "MU": (0,0), "BABA": (0,0), "IQ": (0,0), "JD": (0,0), "TCEHY": (0,0), "APRN": (0,0)}
+		for ticker in tickers:
+			sqlDfBear = spark.sql("SELECT COUNT(*) FROM table WHERE sent='Bearish' AND ticker='"+ticker+"'")
+			countListBear = sqlDfBear.select("*").collect()[0].asDict()
+			print("\n Info:",ticker,countListBear["count(1)"],"\n")
+
+			sqlDfBull = spark.sql("SELECT COUNT(*) FROM table WHERE sent='Bullish' AND ticker='"+ticker+"'")
+			countListBull = sqlDfBull.select("*").collect()[0].asDict()
+
+			tickers[ticker] = (countListBear["count(1)"], countListBull["count(1)"])
+
+		print("\n date dict:",tickers,"\n")
+		d[f] = tickers
+
+	print(d)	
 	#counts = textFiles.flatMap(lambda line: line.lower().split()).map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y)
 #	print(counts.top(10, lambda t: t[1]))
 
