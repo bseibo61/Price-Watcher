d8d2cae91ada93583b4df64040622557ece8677b
bseibo61 <brseibol@buffalo.edu>
2018-11-28 21:51:24 -0500

    better sent formatting

--- aggregateSent.py
+++ aggregateSent.py
@@ -4,4 +4,17 @@ from pprint import pprint
 with open('result.json') as f:
     data = json.load(f)
 
-pprint(data)
+tickers = {"AMD": {}, "TLRY": {}, "MU": {}, "BABA": {}, "IQ": {}, "JD": {}, "TCEHY": {}, "APRN": {}}
+
+print("data length is",len(data))
+
+for date in data:
+    for ticker in data[date]:
+        innerDict = {date: data[date][ticker]}
+        tickers[ticker][date] = data[date][ticker]
+        print(ticker)
+
+pprint(tickers)
+
+with open('Revisedresult.json','w') as fp:
+		json.dump(tickers,fp)
